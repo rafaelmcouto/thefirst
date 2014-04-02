90212d1e0f65e4a2d7c104c8c463522d84afd3bf
Rafael Couto <rafael@rafmac.local>
2014-04-02 18:49:51 -0300

    Diretorio 'gera_dados' renomeado para 'bin'. Inclusao do diretorio 'data'. Acrescentado o modulo para envio de mails no contato

---
+++ bin/contatos.py
@@ -0,0 +1,40 @@
+"""
+    Este codigo trata a entrada de arquivo para a criacao de usuarios no zabbix.
+    Ele recebe como parametro um arquivo de texto no seguinte formato
+    Nome Sobrenome=nome.sobrenome@mail.com
+
+    Ele separa na ordem necessaria e retorna uma lista para fazer o input dos dados usando a API do Zabbix.
+"""
+from gera_senha import id_generator
+
+vetor = []
+indice = "@"
+
+def log_and_pass(filename):
+    #recebe um arquivo como parametro. Dessa forma, pode-se fazer um arquivo por equipe.
+    try:
+        with open (filename) as contatof:
+            for dados in contatof:
+                #faz o split e separa o nome do mail
+                (funcionario, mail) = dados.strip().split('=')
+                #localiza a posicao do @ para pegar o prefixo do mail
+                index_arroba = mail.find(indice)
+                login = mail[0:index_arroba]
+                #gera uma senha aleatoria
+                senha = id_generator(9)
+                #faz o split do nome  sobrenome para fazer o input no Zabbix
+                (nome, sobrenome) = funcionario.strip().split()
+
+                #cria um vetor com a ordem correta
+                vetor.append(login)
+                vetor.append(senha)
+                vetor.append(nome)
+                vetor.append(sobrenome)
+
+        return (vetor)
+        
+    except FileNotFoundError as fnfe:
+        print ("O arquivo nao foi encontrado " + str(fnfe))
+
+if __name__ == "__main__":
+   algo = log_and_pass("rafael.txt")
---
+++ bin/zabbix_screen.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import sys, os
+import smtplib
+
+# Este modulo gera o login a partir do prefixo do mail e a senha
+from contatos import log_and_pass
+
+from zabbix_api import ZabbixAPI
+from argparse import ArgumentParser
+
+debug_flag = False
+progname = os.path.basename(sys.argv[0])
+
+def error(msg) :
+    sys.stderr.write('%s:%s\n' % (progname, msg))
+    sys.exit(255)
+
+def debug(msg) :
+    if debug_flag :
+        sys.stderr.write('%s:DEBUG:%s\n' % (progname, msg))
+
+parser = ArgumentParser(description = 'Create Zabbix Screen with specified criteria')
+
+parser.add_argument('--url', dest = 'url', default = 'http://zabbix.dev.globoi.com/', help = 'Zabbix server address')
+parser.add_argument('-u', '--user', dest = 'user', default = 'Admin', help = 'Zabbix user')
+parser.add_argument('-p', '--password', dest = 'password', default = 'zabbix', help = 'Zabbix password')
+parser.add_argument('-n', '--name', dest = 'name', default = 'Rafael', help = 'Zabbix alias')
+parser.add_argument('-ln', '--last_name', dest = 'last_name', default = 'Couto', help = 'Zabbix last name')
+parser.add_argument('-t', '--team', dest = 'team', default = 'OpBackup', help = 'Zabbix team')
+
+args = parser.parse_args()
+
+zapi = ZabbixAPI(server = args.url, path = "", log_level = 0)
+zapi.login(args.user, args.password)
+
+#voce precisa especificar o arquivo no path local
+info = log_and_pass("arquivo.txt")
+
+#print apenas para se certificar da execucao correta
+print ("Informacoes processadas: ", info)
+
+num = 0
+
+while num < len(info):
+    destinatario = 'seumail@gmail.com'
+    para = info[num] + "@seumail.com"
+    mensagem = "Ola " + info[num + 2] + " " + info[num + 3] + ", estamos enviando a sua senha do Zabbix. Seu login e: " + info[num] + " e sua senha e: " + info[num + 1] + ". Acesse atraves da url - http://zabbix.dev.globoi.com"
+
+    usuario = 'seuemail@gmail.com'
+    senha = 'sua_senha' 
+ 
+    server = smtplib.SMTP('smtp.gmail.com:25')
+    server.starttls() 
+    server.login(usuario,senha) 
+    server.sendmail(destinatario, para, mensagem)
+    
+    num += 4
+
+print ("\nOs mails foram enviados.")    
+server.quit()
+
+num = 0
+
+#Remove a posicao 0 da lista, pegando assim sempre a sequencia correta
+#Neste caso, o 'usrgrpid' esta sendo usado como uma constante. Basta passar o id correto.
+while num < len(info):
+    zapi.user.create({
+                "alias": info.pop(0),
+                "passwd": info.pop(0),
+                "name": info.pop(0),
+                "surname": info.pop(0),
+                "usrgrps": [{
+                        "usrgrpid": "xx"
+                        }],
+                })
